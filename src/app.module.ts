96f9d6a7d9c09cf7b27c8f9ddf491f4b540e078f
joao966 <joaooliveira966@gmail.com>
2022-08-20 22:33:58 -0300

    ADD: initi endpoint questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestionsModule } from './questions/questions.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [QuestionsModule, MongooseModule.forRoot('mongodb://localhost/nest')],
 })
 export class AppModule {}
