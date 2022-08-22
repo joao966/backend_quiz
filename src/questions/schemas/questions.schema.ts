ce9d7d458faba3bc5fb55ddbe5c947f2f60b9c89
joao966 <joaooliveira966@gmail.com>
2022-08-21 23:26:54 -0300

    ADD: schema

---
+++ src/questions/schemas/questions.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type questionsDocument = questions & Document;
+
+@Schema()
+export class questions {
+  @Prop()
+  questions: string[];
+
+  @Prop()
+  answers: string;
+}
+
+export const questionsSchema = SchemaFactory.createForClass(questions);
